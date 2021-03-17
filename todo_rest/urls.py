0fabab404e4939e4a3b28f9c384a4a7207be850f
moshiulrabbi <mhrabby61@gmail.com>
2021-03-17 19:59:02 +0600

    django_rest-todo_added

--- todo_rest/urls.py
+++ todo_rest/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.overview, name = "overview" ),
     path("task-list/",views.tasklist, name="tasklist"),
-    path("task-detail/<int:pk>/",views.taskdetail, name="taskdetail")
+    path("task-detail/<int:pk>/",views.taskdetail, name="taskdetail"),
+    path("task-create/",views.taskcreate,name= "taskcreate"),
+    path("task-update/<int:pk>",views.taskupdate,name= "taskupdate"),
+    path("task-delete/<int:pk>",views.taskdelete,name="taskdelete")
 ]
--- todo_rest/views.py
+++ todo_rest/views.py
@@ -41,3 +41,32 @@ def taskdetail(request,pk):
     tasks = Task.objects.get(id=pk)
     serializer = TaskSerializer(tasks,many=False)
     return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def taskcreate(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    
+    return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def taskupdate(request,pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def taskdelete(request,pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return Response('Item Deleted')
