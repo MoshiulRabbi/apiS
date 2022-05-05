50869142f40ed4b16c37f3ed80f95276eb1aae62
moshiulrabbi <mhrabby61@gmail.com>
2022-05-05 22:17:18 +0600

    fixedTodoOrderedList ::sparkles:

--- todo_rest/views.py
+++ todo_rest/views.py
@@ -30,7 +30,7 @@ def overview(request):
 
 @api_view(['GET'])
 def tasklist(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-id')
     serializer = TaskSerializer(tasks,many=True)
     return Response(serializer.data)
 
